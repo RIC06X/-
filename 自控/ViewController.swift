f380d068f1250bfb0c53c880ad23905d1fffc657
Yanjie Xu <yanjiexu06@gmail.com>
2020-07-09 19:22:42 +0800

    Initial Commit

---
+++ 自控/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  自控
+//
+//  Created by Yanjie Xu on 2020/7/9.
+//  Copyright © 2020 Yanjie Xu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
